2241bc0198e486a2c89e253cbd0cef37757df0df
thaysgalo <thays_resende14@hotmail.com>
2021-02-01 10:19:28 -0300

    refactoring

--- implementacao/Monitor/Monitor.ino
+++ implementacao/Monitor/Monitor.ino
@@ -1,7 +1,7 @@
 #include "ServicoRede.h"
 #include "Valvula.h"
 
-ServicoRede *servicoRede = new ServicoRede("CAYO", "peta1234", "192.168.0.17", 8080);
+ServicoRede* servicoRede = new ServicoRede("CAYO", "peta1234", "192.168.0.17", 8080);
 Valvula valvula(5, servicoRede, "/valvula/consulta-periodo");
 
 void setup()
