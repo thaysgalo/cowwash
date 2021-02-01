667dccfd566d0dd2c69424c69e974313322c6028
thaysgalo <thays_resende14@hotmail.com>
2021-02-01 00:57:43 -0300

    Criacao do projeto web

--- implementacao/web/Valvula.java
+++ implementacao/web/Valvula.java
@@ -8,16 +8,16 @@ import javax.servlet.ServletException;
 
 public class Valvula extends HttpServlet {
 	public void doGet(HttpServletRequest requisicaoHTTP, HttpServletResponse respostaHTTP) throws ServletException, IOException {
-		PrintWriter escritorWeb = respostaHTTP.getWriter();
-		respostaHTTP.setContentType("application/json");
+        PrintWriter escritorWeb = respostaHTTP.getWriter();
+        respostaHTTP.setContentType("application/json");
         respostaHTTP.setCharacterEncoding("UTF-8");
-        
+
         // OS VALORES RETORNADOS ABAIXO ("1" E "2") DEVERÃO VIR DO BANCO DE DADOS.
         String query = requisicaoHTTP.getQueryString();
         if (query.contains("aberto"))
-            escritorWeb.print("1");
+        escritorWeb.print("1");
         else
-            escritorWeb.print("2");
-		escritorWeb.flush();
+        escritorWeb.print("2");
+        escritorWeb.flush();
 	}
 }
