b46814ee13de02e1276ebb93341cc553283e660d
thaysgalo <thays_resende14@hotmail.com>
2021-02-02 13:59:15 -0300

    Alteracao nos métodos de abrir e fechar (classe Valvula) para conectar e desconectar da rede Wifi após a requisição HTTP

--- implementacao/Monitor/Valvula.cpp
+++ implementacao/Monitor/Valvula.cpp
@@ -7,21 +7,26 @@ Valvula::Valvula(int pino, ServicoRede* servicoRede, const char* endereco)
         this->endereco = endereco;
 }
 
-void Valvula::abrir()
+void
+Valvula::abrir()
 {
         servicoRede->conectar();
         digitalWrite(pino, HIGH);
         delay(obterPeriodo(Estado::ABERTO) * 10000);
+        servicoRede->desconectar();
 }
 
-void Valvula::fechar()
+void
+Valvula::fechar()
 {
         servicoRede->conectar();
         digitalWrite(pino, LOW);
         delay(obterPeriodo(Estado::FECHADO) * 10000);
+        servicoRede->desconectar();
 }
 
-int Valvula::obterPeriodo(Estado estado)
+int
+Valvula::obterPeriodo(Estado estado)
 {
         return (estado == Estado::ABERTO ? servicoRede->obterDados(endereco, "aberto").toInt() : servicoRede->obterDados(endereco, "fechado").toInt());
 }
