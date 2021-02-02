fe1c1b2ce3a6a96b7f3acd1c9d71272991e9ebe8
thaysgalo <thays_resende14@hotmail.com>
2021-02-02 14:03:47 -0300

    Remoção do ponteiro do objeto servicoRede.

--- implementacao/Monitor/Monitor.ino
+++ implementacao/Monitor/Monitor.ino
@@ -1,17 +1,17 @@
 #include "ServicoRede.h"
 #include "Valvula.h"
 
-ServicoRede* servicoRede = new ServicoRede("CAYO", "peta1234", "192.168.0.17", 8080);
-Valvula valvula(5, servicoRede, "/valvula/consulta-periodo");
+ServicoRede servicoRede("CAYO", "peta1234", "192.168.0.17", 8080);
+Valvula valvula(5, &servicoRede, "/valvula/consulta-periodo");
 
 void setup()
 {
-        // USAR A LINHA ABAIXO SÓ QUANDO FOR UTILIZAR O SERIAL MONITOR.
+        // DESCOMENTAR A LINHA ABAIXO APENAS QUANDO FOR UTILIZAR O SERIAL MONITOR.
         //Serial.begin(115200);
 }
 
 void loop() 
 {
-          valvula.abrir();
-          valvula.fechar();
+        valvula.abrir();
+        valvula.fechar();
 }
